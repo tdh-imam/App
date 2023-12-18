d4a5e9cdc323fefa79a71fccdcd3e1fc7b0d181d
Aung Kyaw Minn <aungkyawminn@onenex.co>
2023-12-18 06:42:29 +0630

    rebuild

---
+++ assets/Loading-0f8108d4.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c,A as t,B as a,a as _}from"./index-db823c43.js";const n={},d=e=>(t("data-v-1b0a7e0e"),e=e(),a(),e),i={class:"custom-loading"},p=d(()=>_("div",{class:"custom-loading-content"},null,-1)),r=[p];function l(e,u){return s(),c("div",i,r)}const m=o(n,[["render",l],["__scopeId","data-v-1b0a7e0e"]]);export{m as L};
